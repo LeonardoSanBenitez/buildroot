871580cf76abd82de94cea9d1f0a6a6042d32397
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-03-11 01:13:38 -0300

    add package for sensor_bh1750

---
+++ package/sensor_bh1750/sensor_bh1750.mk
@@ -0,0 +1,12 @@
+SENSOR_BH1750_MODULE_VERSION = 1.0
+SENSOR_BH1750_SITE = ../sensor_bh1750_linux_driver
+SENSOR_BH1750_SITE_METHOD = local
+SENSOR_BH1750_LICENSE = GPLv2
+SENSOR_BH1750_LICENSE_FILES = COPYING
+
+define KERNEL_MODULE_BUILD_CMDS
+$(MAKE) -C '$(@D)' LINUX_DIR='$(LINUX_DIR)' CC='$(TARGET_CC)' LD='$(TARGET_LD)' modules
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
